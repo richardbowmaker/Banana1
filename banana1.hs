2283dbd3cc3d869e44561e174ddd23220ffe7244
richardbowmaker <rjb.accounts@live.co.uk>
2017-12-17 18:12:10 +0000

    start of day

--- banana1.hs
+++ banana1.hs
@@ -15,13 +15,17 @@ import Graphics.UI.WXCore hiding (Event)
 main = start mainGUI
 
 
-makeNetworkDescription :: AddHandler Point -> Button () -> MomentIO ()
-makeNetworkDescription addMouseEvent button = do
-    eMouse <- fromAddHandler addMouseEvent
-    bDownAt <- stepper (Point 0 0) eMouse
-    let eMouse' = ((+1) <$ eMouse)
-    bMouse <- accumB 0 eMouse' 
-    eMouseChanged <- changes bMouse
+makeNetworkDescription :: AddHandler Point -> AddHandler Point -> Button () -> MomentIO ()
+makeNetworkDescription addMouseClick addMouseDrag button = do
+
+    eMouseClick <- fromAddHandler addMouseClick
+    bDownAt <- stepper (Point 0 0) eMouseClick
+
+    eMouseDrag <- fromAddHandler addMouseClick
+    bDragTo <- stepper (Point 0 0) eMouseDrag
+    
+
+
     eDownAt <- changes bDownAt
     reactimate' $ fmap (\(Point x y) -> (set button [text := "x y (1) " ++ (show x) ++ " " ++(show y)])) <$> eDownAt
  
@@ -34,29 +38,39 @@ mainGUI = do
     p <- panelCreate f idAny rectNull 0
     t <- staticTextCreate p idAny "0" rectNull 0          
     b1 <- buttonCreate p idAny "" rectNull 0
+    b2 <- buttonCreate p idAny "" rectNull 0
    
+    set f [ text := "Bubble Sim", 
+            bgcolor     := white, 
+            layout      := space 500 500
+          ]
   
    -- set the layout
     windowSetLayout f (fill (container p (margin 5 (column 5 [widget t, widget b1]))))
        
-    (addMouseEvent, fireMouse) <- newAddHandler
-    network <- compile (makeNetworkDescription addMouseEvent b1) 
+    (addMouseClick, fireMouseClick) <- newAddHandler
+    (addMouseDrag, fireMouseDrag) <- newAddHandler
+    network <- compile (makeNetworkDescription addMouseClick addMouseDrag b1) 
     actuate network   
     
-    set f [ text := "Bubble Sim", 
-            bgcolor     := white, 
-            layout      := space 500 500
-           ]
-           
-    set b1 [text := "Click me !",         
-            on click    := onMouseLeftClick b1 fireMouse
-          ]
-       
-  
+            
+    set p [     
+            on click    := onMouseClick fireMouseClick,
+            on drag     := onMouseDrag fireMouseDrag
+          ] 
+ 
+
+ 
     return ()
     
+
     where 
-        onMouseLeftClick :: Button () -> Handler Point -> Point -> IO ()
-        onMouseLeftClick b h p = do
+        onMouseClick :: Handler Point -> Point -> IO ()
+        onMouseClick h p = do
             h p
             return ()
+            
+        onMouseDrag :: Handler Point -> Point -> IO ()
+        onMouseDrag h p = do
+            h p
+            return ()            
